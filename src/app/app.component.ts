9c373593a8e51e82e84d1f11286cfab1a35116be
vkuppan <vkuppan@manh.com>
2021-08-24 12:07:22 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angular-dev';
+  ariaLabel = "ManualGroupIdStartRange=1;ManualGroupIdEndRange=11;SPOPOCreateDateUpdateNote=Note displayed on SPO PO Create Date Popup;LocationPOEdit=Feature flag controlling if Location & PO Information popup is enabled for an organization;InvalidLocationError=Error message displayed in Update Location & PO Information popup if an invalid location is input.;NoSupplyFoundError=Error message displayed in Update Location & PO Information popup if a supply record is not found for the input values.;InvalidSupplyFoundError=Error message displayed in Update Location & PO Information popup if the supply record for the input values does not match the order line ItemId."
+
+  constructor() { }
+
+  ngOnInit(): void {
+   console.log("AppComponent");
+   
+  }
 }
+
+// service.ts
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { NgModule } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import {
+  CUSTOM_ELEMENTS_SCHEMA, NgModule
+} from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { NgxJsonViewerModule } from 'ngx-json-viewer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxDatatableModule,
+    CommonModule,
+    FlexLayoutModule,
+    NgxJsonViewerModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [DatePipe],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
