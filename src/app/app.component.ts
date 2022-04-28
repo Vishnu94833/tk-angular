37893bda191df180d477c3e341a816fd1e5c9295
vkuppan <vkuppan@manh.com>
2022-04-28 21:19:42 +0530

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import GotHouseService from './services';
 
 @Component({
   selector: 'app-root',
@@ -11,20 +10,11 @@ export class AppComponent implements OnInit {
   title = 'angular-dev';
   ariaLabel = "ManualGroupIdStartRange=1;ManualGroupIdEndRange=11;SPOPOCreateDateUpdateNote=Note displayed on SPO PO Create Date Popup;LocationPOEdit=Feature flag controlling if Location & PO Information popup is enabled for an organization;InvalidLocationError=Error message displayed in Update Location & PO Information popup if an invalid location is input.;NoSupplyFoundError=Error message displayed in Update Location & PO Information popup if a supply record is not found for the input values.;InvalidSupplyFoundError=Error message displayed in Update Location & PO Information popup if the supply record for the input values does not match the order line ItemId."
 
-  constructor(private getService: GotHouseService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    let list = this.ariaLabel.split(';');
-
-    list.forEach(e => {
-      let mewList = e.split('=');
-      // console.log({ "KEY": mewList[0], "VALUE": mewList[1] });
-      this.getService.configStoreData = {
-        ...this.getService.configStoreData,
-        [mewList[0]]: mewList[1]
-      }
-    })
-
+   console.log("AppComponent");
+   
   }
 }
 
