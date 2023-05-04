0a4cd91d1445b89d226d5e3619aef55d9e95a94e
vkuppan <vkuppan@manh.com>
2023-05-04 20:09:18 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,14 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormExampleComponent } from './components/reactive-form-example/reactive-form-example.component';
 import { Subform1Component } from './components/reactive-form-example/subform1/subform1.component';
 import { UniversityImpl } from './design-patterns/abstract-example/university-impl';
 import { UniversityInterface } from './design-patterns/abstract-example/university.interface';
-import { MaterialModule } from './material.module';
 import { JsonPlaceHolderInterceptor } from './jsonplaceholder/services/jsonplaceholder-http.interceptor';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [AppComponent, Subform1Component, ReactiveFormExampleComponent],
@@ -24,7 +23,6 @@ import { JsonPlaceHolderInterceptor } from './jsonplaceholder/services/jsonplace
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    NgxDatatableModule,
     CommonModule,
     FlexLayoutModule,
     AppRoutingModule,
